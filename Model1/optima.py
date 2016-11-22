484c8687c5609e1b850e83b4f729ebf4d29c2e92
Maximilian Mordig <maximilian.mordig@epfl.ch>
2016-11-22 11:22:24 +0100

    Completed part 1 and 2, will add non-linearity

--- Model1/optima.py
+++ Model1/optima.py
@@ -27,8 +27,31 @@ B_TRAIN_TIME = Beta('B_TRAIN_TIME',0,-10000,10000,0)
 B_WALK_DIST = Beta('B_WALK_DIST',0,-10000,10000,0)
 B_COST_TRAIN = Beta('B_COST_TRAIN',0,-10000,10000,0)
 B_COST_CAR = Beta('B_COST_CAR',-0,-10000,10000,0)
-B_MALE_CAR = Beta('B_MALE_CAR',0,-10000,10000,0)
-B_MALE_TRAIN = Beta('B_MALE_TRAIN',0,-10000,10000,0)
+#B_MALE_CAR = Beta('B_MALE_CAR',0,-10000,10000,0)
+#B_MALE_TRAIN = Beta('B_MALE_TRAIN',0,-10000,10000,0)
+B_TRIP_PURPOSE = Beta('B_TRIP_PURPOSE',0,-10000,10000,0)
+#B_URBAN_RURAL = Beta('B_URBAN_RURAL',0,-10000,10000,0)
+#B_TYPE_COMMUNE_TRAIN = Beta('B_TYPE_COMMUNE_TRAIN',0,-10000,10000,0)
+#B_TYPE_COMMUNE_CAR1 = Beta('B_TYPE_COMMUNE_CAR1',0,-10000,10000,1)
+#B_TYPE_COMMUNE_CAR2 = Beta('B_TYPE_COMMUNE_CAR2',0,-10000,10000,1)
+B_LANG_CODE = Beta('B_LANG_CODE',0,-10000,10000,0)
+#B_AGE = Beta('B_AGE',0,-10000,10000,0)
+#B_MOTHER_TONGUE = Beta('B_MOTHER_TONGUE',0,-10000,10000,0), same as lang code
+
+# non-linear
+B_CAR_AVAIL1 = Beta('B_CAR_AVAIL1',0,-10000,10000,0)
+B_CAR_AVAIL2 = Beta('B_CAR_AVAIL2',0,-10000,10000,0)
+B_CAR_AVAIL3 = Beta('B_CAR_AVAIL3',0,-10000,10000,0)
+B_FreqCarPar1 = Beta('B_FreqCarPar1',0,-10000,10000,1)
+B_FreqCarPar2 = Beta('B_FreqCarPar2',0,-10000,10000,0)
+B_FreqCarPar34 = Beta('B_FreqCarPar3',0,-10000,10000,0)
+#B_FreqCarPar4 = Beta('B_FreqCarPar4',0,-10000,10000,0)
+
+# Interaction
+#B_INCOME_OVER_PRICE1 = Beta('B_INCOME_OVER_PRICE1',0,-10000,10000,0)
+#B_INCOME_OVER_PRICE2 = Beta('B_INCOME_OVER_PRICE2',0,-10000,10000,0)
+
+
 # parameters relevant to the nests
 #MU_classic = Beta('MU_classic',1,1,10,0)
 
@@ -38,14 +61,36 @@ one  = DefineVariable('one',1)
 CAR_TIME  = DefineVariable('TimeCar1', TimeCar)
 TRAIN_TIME  = DefineVariable('TimePT1', TimePT)
 DISTANCE  = DefineVariable('DISTANCE1', distance_km)
-TRAIN_COST  = DefineVariable('TRAIN_COST1', CostPT)
+TRAIN_COST  = DefineVariable('TRAIN_COST1', MarginalCostPT)
 CAR_COST = DefineVariable('CAR_COST1', CostCarCHF)
-MALE = DefineVariable('MALE', Gender == 1)
+#MALE = DefineVariable('MALE', Gender == 1)
+TRIP_PURPOSE = DefineVariable('TRIP_PURPOSE1', TripPurpose)
+#URBAN_RURAL = DefineVariable('URBAN_RURAL1', UrbRur)
+#TYPE_COMMUNE_TRAIN = DefineVariable('TYPE_COMMUNE_TRAIN1', TypeCommune)
+#TYPE_COMMUNE_CAR1 = DefineVariable('TYPE_COMMUNE_CAR1', TypeCommune == 1)
+#TYPE_COMMUNE_CAR2 = DefineVariable('TYPE_COMMUNE_CAR2', TypeCommune != 1)
+LANG_CODE = DefineVariable('LANG_CODE1', LangCode)
+#AGE = DefineVariable('AGE1', age)
+#MOTHER_TONGUE = DefineVariable('MOTHER_TONGUE1', Mothertongue)
+CAR_AVAIL1 = DefineVariable('CAR_AVAIL1',CarAvail == 1)
+CAR_AVAIL2 = DefineVariable('CAR_AVAIL2',CarAvail == 2)
+CAR_AVAIL3 = DefineVariable('CAR_AVAIL3',CarAvail == 3)
+FREQ_CAR_PAR1 = DefineVariable('FREQ_CAR_PAR1', FreqCarPar == 1)
+FREQ_CAR_PAR2 = DefineVariable('FREQ_CAR_PAR2', FreqCarPar == 2)
+FREQ_CAR_PAR34 = DefineVariable('FREQ_CAR_PAR34', FreqCarPar >= 3)
+#FREQ_CAR_PAR4 = DefineVariable('FREQ_CAR_PAR4', FreqCarPar == 4)
+
+# Interaction
+#INCOME_OVER_PRICE = DefineVariable('INCOME_OVER_PRICE1',Income/)
+
+
 #########################################################################################################
 #[Utilities]
-V1 = ASC_TRAIN * one + B_TRAIN_TIME * TRAIN_TIME + B_COST_TRAIN * TRAIN_COST 
-V2 = ASC_CAR * one + B_CAR_TIME * CAR_TIME + B_COST_CAR * CAR_COST + B_MALE_CAR * MALE
-V3 = ASC_WALK * one + B_WALK_DIST *  DISTANCE + B_MALE_TRAIN * MALE
+
+#  + B_INCOME_OVER_PRICE2 * CAR_COST/Income
+V1 = ASC_TRAIN * one + B_TRAIN_TIME * TRAIN_TIME + B_COST_TRAIN * TRAIN_COST + B_LANG_CODE * LANG_CODE
+V2 = ASC_CAR * one + B_CAR_TIME * CAR_TIME + B_COST_CAR * CAR_COST + B_CAR_AVAIL1 * CAR_AVAIL1 + B_CAR_AVAIL2 * CAR_AVAIL2 + B_CAR_AVAIL3 * CAR_AVAIL3 + B_FreqCarPar1*FREQ_CAR_PAR1 + B_FreqCarPar2*FREQ_CAR_PAR2 + B_FreqCarPar34*FREQ_CAR_PAR34
+V3 = ASC_WALK * one + B_WALK_DIST * DISTANCE + B_TRIP_PURPOSE * TRIP_PURPOSE+ B_LANG_CODE * LANG_CODE
 
 __V = {1: V1, 2: V2, 3: V3}
 
